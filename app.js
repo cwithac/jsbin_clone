0214ddc22b3fae8b2450080c63db2db11fe63eda
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-09 12:19:16 -0500

    Output panel updates with textarea input

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ $( () => {
     $(this).toggleClass('active');
   });
 
+  $('textarea').on('change keyup paste', function() {
+    $('iframe').contents().find('html').html($('#html-panel').val());
+  })
+
+
+
   $('.panel').height($(window).height() - $('nav').height() - 30);
   $('.panel').width(($(window).width() / 2) - 10);
+
 
 });//
