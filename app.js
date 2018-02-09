933726602edaab42b3b54619d626965bb23d193d
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-09 14:36:34 -0500

    Generate panel based on active toggle

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ $( () => {
 
   $('.toggleButton').click(function() {
     $(this).toggleClass('active');
+    const $panelID = $(this).attr('id') + '-panel';
+    $('#' + $panelID).toggleClass('hidden');
+    const $activeCount = 4 - $('.hidden').length;
+    $('.panel').width(($(window).width() / $activeCount) - 10);
   });
 
   $('textarea').on('change keyup paste', function() {
@@ -14,6 +18,7 @@ $( () => {
 
 
 
+//Page Load with Default 2 Active
   $('.panel').height($(window).height() - $('nav').height() - 30);
   $('.panel').width(($(window).width() / 2) - 10);
 
